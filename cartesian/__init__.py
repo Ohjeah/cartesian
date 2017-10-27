17ff95ad8d2ded8dfa71419216e5885f8e1fdb66
mq <markus.quade@gmail.com>
2017-10-27 21:00:30 +0200

    use versioneer and travis to deploy to pypi

--- cartesian/__init__.py
+++ cartesian/__init__.py
@@ -1,1 +1,5 @@
 from .sklearn_api import Symbolic
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -3,34 +3,31 @@ import os
 import sys
 from shutil import rmtree
 
+import versioneer
 from setuptools import find_packages, setup, Command
 
 NAME = "cartesian"
-DESCRIPTION = 'Minimal cartesian genetic programming for symbolic regression.'
-URL = 'https://github.com/ohjeah/cartesian'
-EMAIL = 'info@markusqua.de'
-AUTHOR = 'Markus Quade'
+DESCRIPTION = "Minimal cartesian genetic programming for symbolic regression."
+URL = "https://github.com/ohjeah/cartesian"
+EMAIL = "info@markusqua.de"
+AUTHOR = "Markus Quade"
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "requirements.txt"), "r") as f:
     REQUIRED = f.readlines()
 
-about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
-    exec(f.read(), about)
-
 
 class PublishCommand(Command):
     """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -40,23 +37,23 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds ...')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds ...")
+            rmtree(os.path.join(here, "dist"))
         except FileNotFoundError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution...")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine...')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine...")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
 
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=versioneer.get_version(),
     description=DESCRIPTION,
     long_description=__doc__,
     author=AUTHOR,
@@ -64,13 +61,13 @@ setup(
     url=URL,
     packages=find_packages(exclude=["test", "example"]),
     install_requires=REQUIRED,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    cmdclass={
-        'publish': PublishCommand,
-    },
+    cmdclass=versioneer.get_cmdclass({
+        "publish": PublishCommand,
+    }),
 )
