328913c7946e8d1172d6b9a011c9a765ef3c85c3
Markus Quade <markus.quade@gmail.com>
2017-12-04 15:08:54 +0100

    use versioneer

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ import os
 import versioneer
 from setuptools import find_packages, setup
 
+
 NAME = "cartesian"
-DESCRIPTION = "Minimal cartesian genetic programming for symbolic regression."
-URL = "https://github.com/ohjeah/cartesian"
-EMAIL = "info@markusqua.de"
-AUTHOR = "Markus Quade"
+DESCRIPTION = 'Minimal cartesian genetic programming for symbolic regression.'
+URL = 'https://github.com/ohjeah/cartesian'
+EMAIL = 'info@markusqua.de'
+AUTHOR = 'Markus Quade'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "requirements.txt"), "r") as f:
     REQUIRED = f.readlines()
+
 
 setup(
     name=NAME,
@@ -24,11 +26,11 @@ setup(
     url=URL,
     packages=find_packages(exclude=["test", "example"]),
     install_requires=REQUIRED,
-    license="MIT",
+    license='MIT',
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     cmdclass=versioneer.get_cmdclass(),
 )
